b874e3dae16b6b19acff6857a59b27aebf79e22c
hanif.omar <hanif.omar@naazpremier.com.my>
2024-01-23 20:24:12 +0800

    feat: change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "HavenBakes",
   description: "Premium Butter Cookies",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
 };
 
 export default function RootLayout({
