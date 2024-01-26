1b67721c1e4440eac2dbc43511060046c124e07d
hanif.omar <hanif.omar@naazpremier.com.my>
2024-01-26 15:00:45 +0800

    chore: modify hero section

--- src/components/homepage/Overview.tsx
+++ src/components/homepage/Overview.tsx
@@ -1,20 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./overview.module.css";
-import Lenis from "@studio-freight/lenis";
+
 import ZoomParallax from "../zoomParallax/ZoomParallax";
 
 const Overview = () => {
-  useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time: any) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
     <div className={styles.main}>
       <ZoomParallax />
