515bd4ba209efc15fc4f82812537e20704ea0868
hanif.omar <hanif.omar@naazpremier.com.my>
2024-01-26 15:01:58 +0800

    chore: modify navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,28 @@
-import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="relative flex justify-center items-center py-8 mx-8 my-20 h-10 w-auto">
-      <Image src="/overview/hb_logotype.svg" alt="HavenBakes Logo" fill />
-    </nav>
+    <header className="flex items-center justify-center z-[999] relative">
+      <nav className="flex items-center justify-center  fixed top-0 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full">
+        <ul className="flex items-center justify-center">
+          <li className="h-3/4 flex items-center justify-center relative">
+            <Link
+              className={
+                "relative flex items-center justify-center transition "
+              }
+              href="/"
+            >
+              <img
+                src="/logo/hb_logotype.svg"
+                alt="HavenBakes Logo"
+                className="h-12 sm:h-16 w-32 sm:w-40"
+              />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
