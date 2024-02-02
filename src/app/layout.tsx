73d88f8fe0df3fcb630fa2d2d1cdaf6d354a3cd4
hanif.omar <hanif.omar@naazpremier.com.my>
2024-02-02 10:28:39 +0800

    fix: favicon bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,7 @@ export const metadata: Metadata = {
   title: "HavenBakes",
   description: "Premium Butter Cookies",
   icons: {
-    icon: ["/favicon.ico?v=4"],
-    apple: ["/apple-touch-icon.png?v=4"],
-    shortcut: ["/apple-touch-icon.png"],
+    icon: "/favicon.ico",
   },
 };
 
