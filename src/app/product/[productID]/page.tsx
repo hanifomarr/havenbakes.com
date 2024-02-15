757a3932b3c8288950b07983d698700c4491eb4f
hanif.omar <hanif.omar@naazpremier.com.my>
2024-02-15 15:37:09 +0800

    feat: create product page

---
+++ src/app/product/[productID]/page.tsx
@@ -0,0 +1,22 @@
+// import ImageGallery from "@/components/ImageGallery";
+// import ProductOverview from "@/components/ProductOverview";
+// import ProductList from "@/components/productList";
+// import Container from "@/components/ui/container";
+// import { product } from "@/data/data";
+// import React from "react";
+
+// const ProductPage = () => {
+//   return (
+//     <Container>
+//       <div className="grid gap-8 md:grid-cols-2 my-[15vh] ">
+//         <ImageGallery />
+//         <ProductOverview />
+//       </div>
+//       <div className=" mb-20">
+//         <ProductList title={"Related Product"} items={product} />
+//       </div>
+//     </Container>
+//   );
+// };
+
+// export default ProductPage;
---
+++ src/app/product/page.tsx
@@ -0,0 +1,15 @@
+// import ProductList from "@/components/productList";
+// import Container from "@/components/ui/container";
+// import { product } from "@/data/data";
+
+// const ProductPage = () => {
+//   return (
+//     <Container>
+//       <div className=" flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 mt-[10vh]">
+//         <ProductList title="Features Product" items={product} />
+//       </div>
+//     </Container>
+//   );
+// };
+
+// export default ProductPage;
